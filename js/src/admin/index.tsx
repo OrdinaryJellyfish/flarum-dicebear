798c56b709d9b555bd8c2efa0954bcdef4ed70ae
Tristian Kelly <ordinaryjellyfish@protonmail.com>
2025-02-18 19:00:55 -0500

    formatting fixes

--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -5,7 +5,7 @@ app.initializers.add('ordinaryjellyfish/flarum-dicebear', () => {
   const toKebabCase = (str: string) => {
     return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
   };
-  
+
   const options = Object.keys(avatarOptions).reduce((acc: { [key: string]: string }, key) => {
     const kebabKey = toKebabCase(key);
     const option = avatarOptions[key as keyof typeof avatarOptions];
@@ -19,15 +19,15 @@ app.initializers.add('ordinaryjellyfish/flarum-dicebear', () => {
       setting: 'ordinaryjellyfish-dicebear.avatar_style',
       label: app.translator.trans('ordinaryjellyfish-dicebear.admin.avatar_style'),
       help: app.translator.trans('ordinaryjellyfish-dicebear.admin.avatar_style_help', {
-        a: <a href="https://www.dicebear.com/styles/" />
+        a: <a href="https://www.dicebear.com/styles/" />,
       }),
       type: 'select',
-      options
+      options,
     })
     .registerSetting({
       setting: 'ordinaryjellyfish-dicebear.api_url',
       label: app.translator.trans('ordinaryjellyfish-dicebear.admin.api_url'),
       help: app.translator.trans('ordinaryjellyfish-dicebear.admin.api_url_help'),
-      type: 'text'
-    })
+      type: 'text',
+    });
 });
